0eb5dc9d46876c92ba4750d96856de508afe2bea
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-01-10 21:25:34 +0800

    Create co6.py

---
+++ co6.py
@@ -0,0 +1,29 @@
+#this is my code
+def merge(a,b):
+	k=[]
+	while len(a)!=0 and len(b)!=0:
+		if a[0]<b[0]:
+			k.append(a[0])
+			a.remove(a[0])
+		else:
+			k.append(b[0])
+			b.remove(b[0])
+	if len(a)==0:
+		k=k+b
+	else:
+		k=k+a
+	return k
+	
+def mergesort(x):
+	if len(x)==0 or len(x)==1:
+		return x
+	else:
+		mid=len(x)//2
+		a=mergesort(x[:mid])
+		b=mergesort(x[mid:])
+	return merge(a,b)
+n=int(input())
+x=list(map(int,input().split()))
+g=mergesort(x)
+for i in range(0,n):
+	print(g[i],end=" ")
