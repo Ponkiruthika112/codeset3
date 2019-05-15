e45e36ad1625c59103c9c01357fed44207a8f1f0
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-15 09:42:28 +0530

    Update rev_mid_words.py

--- rev_mid_words.py
+++ rev_mid_words.py
@@ -1,4 +1,5 @@
 # your code goes 
+# your code goes 
 s=input()
 k=s.split()
 p=""
@@ -6,6 +7,8 @@ for i in range(1,len(k)-1):
 	p=p+k[i][::-1]+" "
 if len(k)==1:
 	print(k[0])
+elif len(k)==2:
+	print(s)
 else:
 	print(k[0]+" "+p+k[-1])
-#rev
+#reverse
