0a14648758f85510b5b5cd19241daef1354e1189
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-15 11:37:38 +0530

    Create missing_ele_line.py

---
+++ missing_ele_line.py
@@ -0,0 +1,27 @@
+# your code goes here
+a=input()
+b=input()
+if a.isdigit()==True and b.isdigit()==True:
+	d=list(a)
+	r=list(b)
+else:
+	d=a.split()
+	r=b.split()
+l=d+r
+p=list(set(l))
+j="1:"
+y="2:"
+
+q=[]
+for i in range(0,len(p)):
+	if d.count(p[i])!=r.count(p[i]):
+		q.append([l.index(p[i]),p[i]])
+q.sort()
+for i in range(0,len(q)):
+	if d.count(q[i][1])>r.count(q[i][1]):
+		j=j+str(q[i][1])+" "
+	else:
+		y=y+str(q[i][1])+" "
+print(j.strip())
+print(y.strip())
+#code
