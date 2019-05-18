17bf1b275be19771e8a48e47ff81fda9da71d0cc
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-18 23:10:50 +0800

    Create sub_str_find.py

---
+++ sub_str_find.py
@@ -0,0 +1,14 @@
+# your code goes here
+a,b=map(str,input().split())
+c=0
+for i in range(0,len(a)):
+	for j in range(i+1,len(a)+1):
+		k=a[i:j]
+		if b.find(k)!=-1:
+			c=c+1
+			break
+if c!=0:
+	print("yes")
+else:
+	print("no")
+ #substr
