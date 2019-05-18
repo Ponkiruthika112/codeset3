21eccd1a44d03f74400c8cd91f339e7fea128315
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-19 00:54:42 +0800

    Update subset_2.py

--- subset_2.py
+++ subset_2.py
@@ -1,8 +1,8 @@
-s=input()
+s,l=map(str,input().split())
 k=""
 for i in range(0,len(s)):
 	for j in range(i+1,len(s)+1):
-		if len(s[i:j])==2:
+		if len(s[i:j])==int(l):
 			k=k+s[i:j]+" "
 print(k.strip())
 #subset
