660ac0d911b3b5b1214d6f7a9755084844551373
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-30 21:10:52 +0800

    Update arrangement.py

--- arrangement.py
+++ arrangement.py
@@ -1,17 +1,19 @@
 # your code goes here
 z,o=map(int,input().split())
-k=["110","101"]
+k=["011","110","101"]
 n=z+o
 c=0
+l=[]
 for i in k:
 	p=n*i
 	s=p[0:n]
-	if s.count("0")==z and s.count("1")==o and s[0]=="1" and s[-1]=="1":
+	if s.count("0")==z and s.count("1")==o:
 		c=c+1
-		l=s
-		break
+		l.append(s)
+		
 if c==0:
-	print(-1)
+    print(-1)
 else:
-	print(l)
-#g
+    l.sort()
+    print(l[0])
+#sd
